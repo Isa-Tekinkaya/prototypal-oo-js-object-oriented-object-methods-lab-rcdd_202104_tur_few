8584afeea72816a57929e5e8d7ba40d2684225ee
Isa Tekinkaya <isatekinkaya@yahoo.de>
2021-06-02 18:52:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ BoardMember.prototype.releasePressStatement = function () {
 }
 
 BoardMember.prototype.sayHi = function () {
-  return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
+  return `Hi, my name is <name>. I am from <homeState>, and I was trained in <training>.`
 }
 sayHi("asd","dsa","weer")
