ae34b341ffbfeacfeda9b836415259f0dc6c4c11
Isa Tekinkaya <isatekinkaya@yahoo.de>
2021-06-02 18:43:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,4 +23,3 @@ BoardMember.prototype.releasePressStatement = function () {
 BoardMember.prototype.sayHi = function () {
   return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.` 
 }
-sayHi("mr. İsa", "Duisburg", "Quran")
